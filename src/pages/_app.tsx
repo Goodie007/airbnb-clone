bc77f131ee968bcad69b6201e0ff0c34779897a1
Aruoture Goodness <aruoturegoodness@gmail.com>
2023-07-05 12:14:16 +0100

    set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, theme } from '@chakra-ui/react'
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from "@chakra-ui/theme";
+
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,8 @@
+import DashboardScreen from '@/features/dashboard/DashboardScreen'
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <DashboardScreen />
+    )
+}
