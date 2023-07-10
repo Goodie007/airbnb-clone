282502394b2666346c1a550a03956102b14b1dba
Aruoture Goodness <aruoturegoodness@gmail.com>
2023-07-10 17:03:32 +0100

    fixes

---
+++ src/components/HomeListingItems.tsx
@@ -0,0 +1,175 @@
+import React from 'react'
+import Home1 from '../assets/imgs/homes1.jpg'
+import Home2 from '../assets/imgs/homes 11.jpg'
+import Home3 from '../assets/imgs/homes10.jpg'
+import Home4 from '../assets/imgs/homes12.jpg'
+import Home5 from '../assets/imgs/homes13.jpg'
+import Home6 from '../assets/imgs/homes14.jpg'
+import Home7 from '../assets/imgs/homes15.jpg'
+import Home8 from '../assets/imgs/homes16.jpg'
+import Home9 from '../assets/imgs/homes17.jpg'
+import Home10 from '../assets/imgs/homes18.jpg'
+import Home11 from '../assets/imgs/homes2.jpg'
+import Home12 from '../assets/imgs/homes3.jpg'
+import Home13 from '../assets/imgs/homes4.jpg'
+import Home14 from '../assets/imgs/homes5.jpg'
+import Home15 from '../assets/imgs/homes6.jpg'
+import Home16 from '../assets/imgs/homes7.jpg'
+import Home17 from '../assets/imgs/homes8.jpg'
+import Home18 from '../assets/imgs/homes9.jpg'
+
+export type HomeListingItems = {
+    image: any;
+    icon?: React.ReactNode;
+    title: string;
+    location: string;
+    date: string;
+    amount: string;
+}
+
+export const homeListing: HomeListingItems[] = [
+    {
+        image: Home1,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home2,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home3,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home4,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home5,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home6,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home7,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home9,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home10,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home11,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home12,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home13,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home14,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home15,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home16,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home17,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home18,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+    {
+        image: Home1,
+        title: 'Bode Thomas Mansion',
+        location: 'Ikeja',
+        date: 'Jun 12 - 15',
+        amount: '$340',
+
+    },
+]
--- src/features/dashboard/DashboardScreen.tsx
+++ src/features/dashboard/DashboardScreen.tsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import DashboardWrapper from "@/components/DashboardWrapper";
 import LowerNavbar from "@/components/subnavbar/LowerNavbar";
 import Picture from '../../assets/imgs/style3.png'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { homeListing } from "@/components/HomeListingItems";
 
 export default function DashboardScreen({ children }: any) {
   return (
     <Box
-      height={'100vh'}
+      
     >
       <DashboardWrapper>
         <Box>
-          Hello      
+          <HStack>
+            <Swiper
+               width={201}
+               spaceBetween={0}
+               scrollbar={{ draggable: true }}
+               slidesPerView={1}
+            >
+              {homeListing.map((items:any) => {
+                return (
+                  <SwiperSlide key={items}>
+                    <Box
+                      bgImage={items.image?.src}
+                      width={'100%'}
+                      height={150}
+                      backgroundPosition="center"
+                      backgroundRepeat="no-repeat"
+                      backgroundBlendMode={'screen'}
+                      backgroundSize={'cover'}
+                      position={'relative'}
+                      overflowX={'hidden'}
+                    ></Box>
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </HStack>     
         </Box>
       </DashboardWrapper>
     </Box>
